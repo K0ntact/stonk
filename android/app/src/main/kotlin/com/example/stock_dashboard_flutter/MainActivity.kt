79537b7e480eff31b9bea26711ef4a5cee7f15d3
K0ntact <duyan5745@gmail.com>
2023-10-31 23:05:50 +0700

    fix: app wont work on Android

--- android/app/src/main/kotlin/com/example/stock_dashboard_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/stock_dashboard_flutter/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.stock_dashboard_flutter
-//
-//import io.flutter.embedding.android.FlutterActivity
-//
-//class MainActivity: FlutterActivity() {
-//}
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
